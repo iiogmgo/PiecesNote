db7b691a84f85552f2480861d4f35d4733ae8cd0
aeyeong <iiogmgo@gmail.com>
2016-04-11 00:07:10 +0900

    create pieces page

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -5,3 +5,8 @@ from flask import make_response, render_template
 class IndexAPI(Resource):
     def get(self):
         return make_response(render_template('index.html'))
+
+
+class PiecesIndexAPI(Resource):
+    def get(self, nation, month):
+        return ''.join(['hi, it is a page for ', nation, ' at ', month])
--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from flask.ext.restful import Api
-from app.controllers.index import IndexAPI
+from app.controllers.index import IndexAPI, PiecesIndexAPI
 
 
 def create_restful_app(app):
     api = Api(app)
     api.add_resource(IndexAPI, '/', endpoint='index')
+    api.add_resource(PiecesIndexAPI, '/<string:nation>/<string:month>', endpoint='pieces_index')
 
     return api
